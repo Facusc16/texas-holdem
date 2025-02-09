870f313d9403d16d814b744088af56749868911c
Facundo Silva Cabrita <facundoa.silvacabrita@gmail.com>
2025-02-09 01:17:02 -0300

    Agregada función repartir cartas

--- texas-holdem.py
+++ texas-holdem.py
@@ -7,8 +7,32 @@ maso = ["[A♠]", "[2♠]", "[3♠]", "[4♠]", "[5♠]", "[6♠]", "[7♠]", "[
         "[A♦]", "[2♦]", "[3♦]", "[4♦]", "[5♦]", "[6♦]", "[7♦]", "[8♦]", "[9♦]", "[10♦]", "[J♦]", "[Q♦]", "[K♦]"]
 
 
+def define_cards():
+    chosen_cards = []
+    user_cards = []
+    machine_cards = []
+    table_cards = []
+
+    while len(chosen_cards) < 9:
+        card = random.choice(maso)
+
+        if card not in chosen_cards:
+            chosen_cards.append(card)
+
+            if len(user_cards) < 2:
+                user_cards.append(card)
+
+            elif len(machine_cards) < 2:
+                machine_cards.append(card)
+
+            else:
+                table_cards.append(card)
+
+    return user_cards, machine_cards, table_cards
+
+
 def main():
-    pass
+    user_cards, machine_cards, table_cards = define_cards()
 
 
 if __name__ == "__main__":
